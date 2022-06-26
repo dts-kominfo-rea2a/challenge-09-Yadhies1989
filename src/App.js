dad65c31cdf2fa4a43a2ee34d00bb653e827d985
yadhies1989 <yadi.sepriadi@gmail.com>
2022-06-26 08:52:09 +0700

    commit1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import './App.css';
+import "./App.css";
+import Contact from "./components/Contact";
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from "./data/contacts.json";
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
-      
+      <Contact data={contacts[0]} />
+      <Contact data={contacts[1]} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,22 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
-import React from 'react';
+import React from "react";
 
-const Contact = () => {
-    return (
-        <></>
-    )
-}
+const Contact = ({ data }) => {
+  return (
+    <div className="card">
+      <div>
+        <img src={data.photo} alt={data.name} className="photo"></img>
+      </div>
+      <div className="isi">
+        <section>
+          <p>{data.name}</p>
+          <p>{data.phone}</p>
+          <p>{data.email}</p>
+        </section>
+      </div>
+    </div>
+  );
+};
 
 export default Contact;
